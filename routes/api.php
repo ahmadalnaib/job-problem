ee3b1b379d0f2329dd273435b0f5663cadc634e3
ahmedhabeeb73 <alnaib888@gmail.com>
2025-06-04 08:21:25 +0200

    add api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\JobApplication;
+use Illuminate\Contracts\Queue\Job;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
+
+Route::get('/jobs', function () {
+$jobs=JobApplication::all();
+return response()->json($jobs);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
