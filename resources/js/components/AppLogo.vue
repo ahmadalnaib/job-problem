d56dc8bc7de910d06cfaa5300f880ab42702f1b2
ahmedhabeeb73 <alnaib888@gmail.com>
2025-06-07 08:30:45 +0200

    update the logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import {Link} from '@inertiajs/vue3';
 </script>
 
 <template>
@@ -7,6 +8,10 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">
+            <Link href="/" >
+                HumanHup
+            </Link>
+        </span>
     </div>
 </template>
--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -13,12 +13,18 @@ defineProps<Props>();
 </script>
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42" :class="className" v-bind="$attrs">
+    <svg xmlns="http://www.w3.org/2000/svg" :class="className" v-bind="$attrs" viewBox="0 0 250 250" fill="none">
+<rect width="250" height="250" fill="black"/>
+<path d="M102.812 115.062V129.688H55.3125V115.062H102.812ZM60.875 78V169H42.125V78H60.875ZM116.188 78V169H97.5V78H116.188ZM193.188 115.062V129.688H145.688V115.062H193.188ZM151.25 78V169H132.5V78H151.25ZM206.562 78V169H187.875V78H206.562Z" fill="white"/>
+<!-- Code injected by live-server -->
+
+</svg>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42" :class="className" v-bind="$attrs">
         <path
             fill="currentColor"
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M17.2 5.633 8.6.855 0 5.633v26.51l16.2 9 16.2-9v-8.442l7.6-4.223V9.856l-8.6-4.777-8.6 4.777V18.3l-5.6 3.111V5.633ZM38 18.301l-5.6 3.11v-6.157l5.6-3.11V18.3Zm-1.06-7.856-5.54 3.078-5.54-3.079 5.54-3.078 5.54 3.079ZM24.8 18.3v-6.157l5.6 3.111v6.158L24.8 18.3Zm-1 1.732 5.54 3.078-13.14 7.302-5.54-3.078 13.14-7.3v-.002Zm-16.2 7.89 7.6 4.222V38.3L2 30.966V7.92l5.6 3.111v16.892ZM8.6 9.3 3.06 6.222 8.6 3.143l5.54 3.08L8.6 9.3Zm21.8 15.51-13.2 7.334V38.3l13.2-7.334v-6.156ZM9.6 11.034l5.6-3.11v14.6l-5.6 3.11v-14.6Z"
         />
-    </svg>
+    </svg> -->
 </template>
