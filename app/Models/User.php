19200bcf382d76ccdce5f835a8cacadb367b1117
ahmedhabeeb73 <alnaib888@gmail.com>
2025-06-26 11:33:08 +0200

    add MustVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use App\Models\JobApplication;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use NotificationChannels\Expo\ExpoPushToken;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable,HasApiTokens;
